e0de5cad99a484e6033ce6a811aea30804a875da
Afnan706 <afnansalvatore@gmail.com>
2025-11-23 23:22:01 -0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// footer year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// dark/light toggle
+const toggle = document.getElementById("themeToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggle.textContent = document.body.classList.contains("light") ? "🌞" : "🌙";
+});
